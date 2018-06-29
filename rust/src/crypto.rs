7631720c9bd058e37b4d4a9673c5b2814b105c1d
Ryan Lehmkuhl <ryanleh.ob@gmail.com>
2018-06-29 16:56:29 -0700

    Add a few function sigs

--- rust/src/crypto.rs
+++ rust/src/crypto.rs
@@ -1,1 +1,25 @@
 extern crate ring;
+
+struct Crypto {
+    hasher: ring::digest::Algorithm,
+    salt: u8,
+    key_hash: u8,
+    cipher_key: u8,
+    mac_key: u8,
+    nonce: u8,
+    ciper: ring::aead::Algorithm,
+}
+
+impl Crypto {
+    fn get_random_bytes(&self, n: i32) -> i32 {
+        0
+    }
+
+    fn derive_key(&self, password: &str) -> u8 {
+        0
+    }
+
+    fn hash(&self, msg: &str) -> u8 {
+
+    }
+}
