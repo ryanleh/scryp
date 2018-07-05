fe30eebbcd83c57daee705e38e26b1eabecc0d03
Ryan Lehmkuhl <ryanleh.ob@gmail.com>
2018-07-05 15:22:53 -0700

    Add verify_key function

--- rust/src/crypto.rs
+++ rust/src/crypto.rs
@@ -1,14 +1,14 @@
-use std::error::Error;
-
 extern crate ring;
-use self::ring::{aead, digest, rand, pbkdf2};
+use self::ring::{aead, digest, rand, pbkdf2, constant_time, error};
 use self::rand::{SystemRandom, SecureRandom};
+
 
 static AEAD_ALG: &'static aead::Algorithm = &aead::AES_256_GCM;
 static DIGEST_ALG: &'static digest::Algorithm = &digest::SHA256;
 const KEY_LEN: usize = 16;
 const SALT_LEN: usize = 16;
 const NONCE_LEN: usize = 16;
+const HASH_LEN: usize = 16;
 const PBKDF2_ITERS: u32 = 100000;
 
 
@@ -21,13 +21,14 @@ pub struct Crypto {
 
 
 impl Crypto {
-    // Weird func sig because of poor optional argument support
     // TODO: add support for choosing hash
-    fn new<'a>(&self, password: &'a str) -> Crypto {
-        let mut salt = [0; SALT_LEN];
+    fn new<'a, T: Into<Option<[u8; SALT_LEN]>>,
+           V: Into<Option<[u8; NONCE_LEN]>>>(&self, password: &'a str, salt: T,
+                                             nonce: V) -> Crypto {
+        let mut salt = salt.into().unwrap_or([0; SALT_LEN]);
         self.get_random_bytes(&mut salt);
 
-        let mut nonce = [0; NONCE_LEN];
+        let mut nonce = nonce.into().unwrap_or([0; NONCE_LEN]);
         self.get_random_bytes(&mut nonce);
         
         let mut cipher_key = [0; KEY_LEN];
@@ -54,9 +55,12 @@ impl Crypto {
                        password.as_bytes(), dest);
     }
 
-    /*fn verify_key(&self, password: &str) -> Result<(), Error> {
-        Some(())
-    }*/
+    fn verify_key(&self, password: &str, file_key_hash: &[u8; HASH_LEN]) -> Result<(), error::Unspecified> {
+        let mut given_key = [0; KEY_LEN];
+        self.derive_key(password, &mut given_key);
+        let given_key_hash = self.hash(&given_key);
+        constant_time::verify_slices_are_equal(given_key_hash.as_ref(), file_key_hash)
+    }
 
     fn hash(&self, msg: &[u8]) -> digest::Digest {
         digest::digest(DIGEST_ALG, msg)
