f3f3226c4aef420da4a50d1c8ea483d871e2a003
Ryan Lehmkuhl <ryanleh.ob@gmail.com>
2018-07-04 18:03:54 -0700

    Implement basic FileHandler structure and read+write functionality

---
+++ rust/src/file_handler.rs
@@ -0,0 +1,36 @@
+use std::fs::File;
+use std::io::prelude::*;
+use ::Operation;
+
+struct FileHandler {
+    content: Vec<u8>,
+    operation: Operation,
+    name: String,
+    to_write_name: String,
+    to_write: Vec<u8>,
+}
+
+impl FileHandler {
+    fn new(name: String, operation: Operation) -> FileHandler {
+        let mut content = Vec::new();
+        File::open(&name)
+            .expect("Error opening file")
+            .read_to_end(&mut content);
+        FileHandler{ content, 
+                     name, 
+                     operation, 
+                     to_write_name: String::new(),
+                     to_write: Vec::new(),
+        } 
+    }
+
+    fn write(&self) {
+        File::create(&self.to_write_name)
+                    .expect("Error creating file (permissions issue?)")
+                    .write(&self.to_write);
+        match self.operation {
+            ref DECRYPT => println!("File: {} decrypted!", self.name),
+            ref ENCRYPT => println!("File: {} encrypted!", self.name),
+        }
+    }
+}
