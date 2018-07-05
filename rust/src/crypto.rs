3b8ba92b6b5f9344a5ebb634a4853f4788be11a2
Ryan Lehmkuhl <ryanleh.ob@gmail.com>
2018-07-05 12:58:36 -0700

    Add hash and pbkdf2 derive function

--- rust/src/crypto.rs
+++ rust/src/crypto.rs
@@ -1,16 +1,19 @@
+use std::error::Error;
+
 extern crate ring;
 use self::ring::{aead, digest, rand, pbkdf2};
 use self::rand::{SystemRandom, SecureRandom};
 
 static AEAD_ALG: &'static aead::Algorithm = &aead::AES_256_GCM;
 static DIGEST_ALG: &'static digest::Algorithm = &digest::SHA256;
-const CREDENTIAL_LEN: usize = digest::SHA256_OUTPUT_LEN;
+const KEY_LEN: usize = 16;
+const PBKDF2_ITERS: u32 = 100000;
 
 
 pub struct Crypto {
     salt: [u8; 16],
     nonce: [u8; 12],
-    key_hash: [u8; 32],
+    key_hash: digest::Digest,
     cipher_key: [u8; 16],
 }
 
@@ -23,11 +26,17 @@ impl Crypto {
         self.get_random_bytes(&mut salt);
         let mut nonce = [0; 12];
         self.get_random_bytes(&mut nonce);
+        
+        let mut cipher_key = [0; KEY_LEN];
+        self.derive_key(password, &mut cipher_key);
+
+        let key_hash = self.hash(&cipher_key);
+
         Crypto {
                 salt,
                 nonce,
-                key_hash: [0; 32],
-                cipher_key: [0; 32],
+                key_hash,
+                cipher_key,
         }
     }
     
@@ -37,14 +46,20 @@ impl Crypto {
             .expect("Failed to fill dest");
     }
 
-    /*fn derive_key(&self, password: &str) -> u8 {
-        0
-    }
-    fn hash(&self, msg: &str) -> u8 {
-        0
+    fn derive_key(&self, password: &str, dest: &mut [u8]) {
+        pbkdf2::derive(DIGEST_ALG, PBKDF2_ITERS, &self.salt,
+                       password.as_bytes(), dest);
     }
 
-    fn aes_encrypt(&self, plaintext: &str) -> String {
+    /*fn verify_key(&self, password: &str) -> Result<(), Error> {
+        Some(())
+    }*/
+
+    fn hash(&self, msg: &[u8]) -> digest::Digest {
+        digest::digest(DIGEST_ALG, msg)
+    }
+
+    /*fn aes_encrypt(&self, plaintext: &str) -> String {
         String::new()
     }
     
