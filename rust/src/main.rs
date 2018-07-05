44559e3cb8a335387fecaf8a06096b2b8b9140ab
Ryan Lehmkuhl <ryanleh.ob@gmail.com>
2018-07-04 18:52:27 -0700

    Add command line arg parsing

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,40 @@
+extern crate scrypto;
+extern crate clap;
+mod file_handler;
+use clap::{Arg, App};
+use scrypto::Operation;
+
+
+
 fn main() {
-    
+    let matches = App::new("scrypto")
+        .author("Ryan Lehmkuhl <ryanleh.ob@gmail.com")
+        .about("Encrypt files using 128-bit AES-GCM")
+        .arg(Arg::with_name("filenames")
+             .help("Files to encrypt/decrypt (Default is encrypt)")
+             .required(true)
+             .multiple(true))
+        .arg(Arg::with_name("decrypt")
+             .help("Sets mode to decrypt")
+             .short("d")
+             .long("decrypt"))
+        .arg(Arg::with_name("remove")
+             .help("Remove original file")
+             .short("r")
+             .long("remove"))
+        .get_matches();
+
+    let operation;
+    if matches.is_present("decrypt") {
+        operation = Operation::DECRYPT;
+    } else {
+        operation = Operation::ENCRYPT;
+    }
+
+    let mut remove = false;
+    if matches.is_present("remove") {
+        remove = true;
+    }
+
+    let filenames: Vec<&str> = matches.values_of("filenames").unwrap().collect();
 }
