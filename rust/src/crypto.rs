ac1d45bb85684d3689087b2e92dfc7e084badd92
Ryan Lehmkuhl <ryanleh.ob@gmail.com>
2018-07-06 22:21:28 -0700

    Finish AES tests.  Reconfigure nonce and tag sizes.  Finish AES functions

--- rust/src/crypto.rs
+++ rust/src/crypto.rs
@@ -3,39 +3,48 @@ use self::ring::{aead, digest, rand, pbkdf2, constant_time, error};
 use self::rand::{SystemRandom, SecureRandom};
 use self::error::Unspecified as CryptoError;
 
-use Operation;
-
 static AEAD_ALG: &'static aead::Algorithm = &aead::AES_256_GCM;
 static DIGEST_ALG: &'static digest::Algorithm = &digest::SHA256;
-const TAG_LEN: usize = aead::MAX_TAG_LEN;
-const KEY_LEN: usize = 16;
+const TAG_LEN: usize = 16;
+const KEY_LEN: usize = 32;
 const SALT_LEN: usize = 16;
-const NONCE_LEN: usize = 16;
+// TODO: Fix nonce parameters
+const NONCE_LEN: usize = 12;
 const HASH_LEN: usize = 32;
 const PBKDF2_ITERS: u32 = 300000;
 
 
-pub struct Crypto<T> {
+pub struct Crypto {
     salt: [u8; SALT_LEN],
     nonce: [u8; NONCE_LEN],
     key_hash: [u8; HASH_LEN],
-    cipher_key: T,
+    cipher_key: [u8; KEY_LEN],
 }
 
 
 impl Crypto {
     fn new<'a, T: Into<Option<[u8; SALT_LEN]>>, V: Into<Option<[u8; NONCE_LEN]>>>
-            (password: &'a str, operation: Operation, salt: T, nonce: V) -> Crypto {
-        let mut salt = salt.into().unwrap_or([0; SALT_LEN]);
-        Crypto::get_random_bytes(&mut salt);
+            (password: &'a str, salt: T, nonce: V) -> Crypto {
+        let mut salt = match salt.into() {
+            Some(s) => s,
+            None => {
+                let mut temp = [0; SALT_LEN];
+                Crypto::get_random_bytes(&mut temp);
+                temp
+            },
+        };
+            
+        let mut nonce = match nonce.into() {
+            Some(s) => s,
+            None => {
+                let mut temp = [0; NONCE_LEN];
+                Crypto::get_random_bytes(&mut temp);
+                temp
+            },
+        };
 
-        let mut nonce = nonce.into().unwrap_or([0; NONCE_LEN]);
-        Crypto::get_random_bytes(&mut nonce);
-
-        let mut key = [0; KEY_LEN];
-        Crypto::derive_key(password, &salt, &mut key);
-        let cipher_key = match operation {
-            ENCRYPT => 
+        let mut cipher_key = [0; KEY_LEN];
+        Crypto::derive_key(password, &salt, &mut cipher_key);
 
         let key_hash = Crypto::hash(&cipher_key);
 
@@ -68,13 +77,19 @@ impl Crypto {
     }
 
     // Perhaps find a better way to handle memory here?
-    fn aes_encrypt<'a>(&self,ciphertext: &'a mut [u8]) -> Result<usize, CryptoError> {
-        //aead::seal_in_place(&self.seal_key, &self.nonce, &[], &mut ciphertext, TAG_LEN)
-        Ok(3)
+    // TODO: Change naming
+    fn aes_encrypt<'a>(&self, mut plaintext: &'a mut [u8]) -> Result<&'a [u8], CryptoError> {
+        let seal_key = aead::SealingKey::new(AEAD_ALG, &self.cipher_key)
+            .expect("Seal keygen failed");
+        let size = aead::seal_in_place(&seal_key, &self.nonce, &[], &mut plaintext, TAG_LEN)
+            .expect("Seal failed");
+        Ok(&plaintext[..size])
     }
     
     fn aes_decrypt<'a>(&self, ciphertext: &'a mut [u8]) -> Result<&'a mut [u8], CryptoError> {
-        Err(error::Unspecified)
+        let open_key = aead::OpeningKey::new(AEAD_ALG, &self.cipher_key)
+            .expect("Open keygen failed");
+        aead::open_in_place(&open_key, &self.nonce, &[], 0, ciphertext)
     }
 }
 
@@ -85,9 +100,7 @@ mod tests {
     // "test" in u8
     const test: [u8; 4] = [116, 101, 115, 116];
     const salt: [u8; SALT_LEN] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2];
-    const nonce: [u8; NONCE_LEN] = [37, 134, 36, 162, 205, 16, 237, 253, 119, 102, 
-                                    189, 36, 173, 122, 192, 107];
-
+    const nonce: [u8; NONCE_LEN] = [0; NONCE_LEN];
     #[test]
     fn test_hash() {
         // Compute SHA256("test")
@@ -101,42 +114,55 @@ mod tests {
     
     #[test]
     fn test_derive_key() {
-        let expected: [u8; KEY_LEN] = [241, 38, 124, 132, 21, 185, 197, 23, 136,
-                                       236, 178, 62, 212, 44, 248, 227];
+        let expected: [u8; KEY_LEN] = [241, 38, 124, 132, 21, 185, 197, 23, 136, 236, 178, 
+                                       62, 212, 44, 248, 227, 0, 225, 58, 160, 25, 62, 112, 
+                                       147, 98, 197, 141, 104, 22, 214, 232, 18];
         let mut actual = [0; KEY_LEN];
         Crypto::derive_key("test", &salt, &mut actual);
         assert_eq!(expected, actual);
     }
 
     #[test]
-    #[should_panic]
     fn test_verify_key() {
-        let correct: [u8; KEY_LEN] = [241, 38, 124, 132, 21, 185, 197, 23, 136,
-                                        236, 178, 62, 212, 44, 248, 227];
+        let correct: [u8; KEY_LEN] = [241, 38, 124, 132, 21, 185, 197, 23, 136, 236, 178, 
+                                       62, 212, 44, 248, 227, 0, 225, 58, 160, 25, 62, 112, 
+                                       147, 98, 197, 141, 104, 22, 214, 232, 18];
         let correct_hash = Crypto::hash(&correct);
         let result = Crypto::verify_key("test", &salt, &correct_hash);
         assert_eq!(result.unwrap(), ());
+    }
 
-        let incorrect: [u8; KEY_LEN] = [240, 38, 124, 132, 21, 185, 197, 23, 136,
-                                        236, 178, 62, 212, 44, 248, 227];
+    #[test]
+    #[should_panic]
+    fn test_verify_incorrect_key() {
+        let incorrect: [u8; KEY_LEN] = [240, 38, 124, 132, 21, 185, 197, 23, 136, 236, 178, 
+                                       62, 212, 44, 248, 227, 0, 225, 58, 160, 25, 62, 112, 
+                                       147, 98, 197, 141, 104, 22, 214, 232, 18];
         let incorrect_hash = Crypto::hash(&incorrect);
         let result = Crypto::verify_key("test", &salt, &incorrect_hash);
         result.unwrap();
     }
 
     #[test]
-    fn test_aes() {
-        let correct = [139, 206, 129, 20, 238, 11, 138, 165, 185, 25, 216, 151, 80, 192, 44, 49, 
-                   78, 31, 42, 168];
+    fn test_aes_encrypt() {
         let crypto = Crypto::new("test", salt, nonce); 
-        let message: String = "test".to_string();
-        let mut ciphertext: Vec<u8> = vec![0;message.len()+TAG_LEN];
+        let mut correct = [161, 199, 190, 204, 106, 148, 112, 203, 127, 207, 65, 77, 59, 48, 130, 
+                           165, 228, 1, 28, 204];
+        let msg: String = "test".to_string();
+        let mut ciphertext: Vec<u8> = vec![0;msg.len()+TAG_LEN];
         ciphertext[..msg.len()].copy_from_slice(msg.as_bytes());
-        let actual = crypto.aes_encrypt(&mut ciphertext);
+        let actual = crypto.aes_encrypt(&mut ciphertext).unwrap();
         assert_eq!(correct, actual);
         
+    }
+
+    #[test]
+    fn test_aes_decrypt() {
+        let crypto = Crypto::new("test", salt, nonce); 
         let correct = "test".to_string();
-        let plaintext = crypto.aes_decrypt(actual).unwrap();
+        let mut ciphertext = [161, 199, 190, 204, 106, 148, 112, 203, 127, 207, 65, 77, 59, 48,
+                              130, 165, 228, 1, 28, 204];
+        let plaintext = crypto.aes_decrypt(&mut ciphertext).unwrap();
         assert_eq!(correct.as_bytes(), plaintext);
     }
 }
